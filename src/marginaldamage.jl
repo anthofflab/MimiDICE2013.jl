8d5bdbcf75d5076c122a0c1f1fe4e368b5b99585
Cora Kingdon <ckingdon@berkeley.edu>
2018-02-27 13:29:11 -0500

    incorporate Frank's comments

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -17,29 +17,49 @@ function getmarginal_dice_models(;emissionyear=2010)
     return m1, m2
 end
 
-function get_dice_scc(parameters; emissionyear=2010, discountrate=0.05)
-    m1 = constructdice(parameters)
-    m2 = constructdice(parameters)
+function get_dice_scc(; discount_type=:Constant, emissionyear=2010, constant=0.03, eta=1.5, prtp=0.015, clim_sens=nothing, parameters=nothing)
 
-    addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+    # Get base and marginal versions of dice
+    if parameters==nothing # Default use of Excel parameters
+        base = getdiceexcel()
+        marginal = getdiceexcel()
+    else
+        base = constructdice(parameters)
+        marginal = constructdice(parameters)
+    end 
+
+    # Override standard parameter settings with climate sensitivity, if provided
+    if clim_sens!=nothing
+        setparameter(base, :climatedynamics, :t2xco2, clim_sens)
+        setparameter(marginal, :climatedynamics, :t2xco2, clim_sens)
+    end 
+
+    # Add the emissions pulse
+    addcomponent(marginal, adder, :marginalemission, before=:co2cycle)
     addem = zeros(60)
     addem[getindexfromyear_dice_2013(emissionyear)] = 1.0
-    setparameter(m2, :marginalemission, :add, addem)
-    connectparameter(m2, :marginalemission, :input, :emissions, :E)
-    connectparameter(m2, :co2cycle, :E, :marginalemission, :output)
+    setparameter(marginal, :marginalemission, :add, addem)
+    connectparameter(marginal, :marginalemission, :input, :emissions, :E)
+    connectparameter(marginal, :co2cycle, :E, :marginalemission, :output)
 
-    run(m1)
-    run(m2)
+    # Run the models
+    run(base)
+    run(marginal)
 
-    scc = compute_scc(m1, m2, discountrate)
+    # Extract damages
+    marginal_damages = marginal[:damages, :DAMAGES] - base[:damages, :DAMAGES]
+
+    # Discounting set up
+    if discount_type == :Constant
+        # caculate constant discounting
+    elseif discount_type == :Ramsey
+        # calculate Ramsey discounting
+    else
+        error("Discount of type $discount_type not supported.")
+    end 
+
+    # Calculate SCC
+    scc = sum(discounted_damages)
 
     return scc 
 end
-
-function compute_scc(m1, m2, discountrate)
-    scc = 0
-    
-    # FRANK's SCC equation loop here:
-
-    return scc
-end 
