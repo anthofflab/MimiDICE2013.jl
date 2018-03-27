a56c4cad3a363b9241b94875db4462c5b39a9ced
Richard Plevin <rich@plevin.com>
2018-03-27 16:53:05 -0700

    Tested updated support for MarginalModel.

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -1,18 +1,44 @@
 include("dice2013.jl")
+using dice2013
 
 function getmarginal_dice_models(;emissionyear=2010)
-    m1 = getdiceexcel()
+    mm = MarginalModel(DICE)
+    m1 = mm.base
+    m2 = mm.marginal
 
-    m2 = getdiceexcel()
     addcomponent(m2, adder, :marginalemission, before=:co2cycle)
-    addem = zeros(60)
-    addem[getindexfromyear_dice_2013(emissionyear)] = 1.0
-    setparameter(m2, :marginalemission, :add, addem)
-    connectparameter(m2, :marginalemission, :input, :emissions, :E)
-    connectparameter(m2, :co2cycle, :E, :marginalemission, :output)
+
+    time = dimension(m1, :time)
+    addem = zeros(length(time))
+    addem[time[emissionyear]] = 1.0
+
+    set_parameter!(m2, :marginalemission, :add, addem)
+    connect_parameter(m2, :marginalemission, :input, :emissions, :E)
+    connect_parameter(m2, :co2cycle, :E, :marginalemission, :output)
 
     run(m1)
     run(m2)
 
     return m1, m2
 end
+
+#
+# Old way
+#
+
+# function getmarginal_dice_models(;emissionyear=2010)
+#     m1 = getdiceexcel()
+
+#     m2 = getdiceexcel()
+#     addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+#     addem = zeros(60)
+#     addem[getindexfromyear_dice_2013(emissionyear)] = 1.0
+#     setparameter(m2, :marginalemission, :add, addem)
+#     connectparameter(m2, :marginalemission, :input, :emissions, :E)
+#     connectparameter(m2, :co2cycle, :E, :marginalemission, :output)
+
+#     run(m1)
+#     run(m2)
+
+#     return m1, m2
+# end
