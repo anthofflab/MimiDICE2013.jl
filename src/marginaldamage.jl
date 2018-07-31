633ea54e3f916bc99af7a58ddfa518304c7b814a
lrennels <lrennels@berkeley.edu>
2018-07-31 14:42:15 -0700

    Update function names

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -6,15 +6,15 @@ function getmarginal_dice_models(;emissionyear=2010)
     m1 = mm.base
     m2 = mm.marginal
 
-    addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+    add_comp!(m2, adder, :marginalemission, before=:co2cycle)
 
     time = dimension(m1, :time)
     addem = zeros(length(time))
     addem[time[emissionyear]] = 1.0
 
-    set_parameter!(m2, :marginalemission, :add, addem)
-    connect_parameter(m2, :marginalemission, :input, :emissions, :E)
-    connect_parameter(m2, :co2cycle, :E, :marginalemission, :output)
+    set_param!(m2, :marginalemission, :add, addem)
+    connect_param!(m2, :marginalemission, :input, :emissions, :E)
+    connect_param!(m2, :co2cycle, :E, :marginalemission, :output)
 
     run(m1)
     run(m2)
@@ -30,7 +30,7 @@ end
 #     m1 = getdiceexcel()
 
 #     m2 = getdiceexcel()
-#     addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+#     add_comp!(m2, adder, :marginalemission, before=:co2cycle)
 #     addem = zeros(60)
 #     addem[getindexfromyear_dice_2013(emissionyear)] = 1.0
 #     setparameter(m2, :marginalemission, :add, addem)
