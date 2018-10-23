d1ce3dc4b74d59dfa27de713a1a8d85558f6cfac
lrennels <lrennels@berkeley.edu>
2018-10-23 14:58:45 -0700

    Remove offset keyword arg

--- src/dice2013.jl
+++ src/dice2013.jl
@@ -39,14 +39,14 @@ function constructdice(p)
     set_param!(m, :grosseconomy, :k0, p[:k0])
 
     # Note: offset=1 => dependence is on on prior timestep, i.e., not a cycle
-    connect_param!(m, :grosseconomy, :I, :neteconomy, :I, offset=1)
+    connect_param!(m, :grosseconomy, :I, :neteconomy, :I)
 
     # EMISSIONS COMPONENT
     set_param!(m, :emissions, :sigma, p[:sigma])
     set_param!(m, :emissions, :MIU, p[:MIU])
     set_param!(m, :emissions, :etree, p[:etree])
     set_param!(m, :emissions, :cca0, p[:cca0])
-    connect_param!(m, :emissions, :YGROSS, :grosseconomy, :YGROSS, offset=0)
+    connect_param!(m, :emissions, :YGROSS, :grosseconomy, :YGROSS)
 
     # CO2 CYCLE COMPONENT
     set_param!(m, :co2cycle, :mat0, p[:mat0])
@@ -59,13 +59,13 @@ function constructdice(p)
     set_param!(m, :co2cycle, :b22, p[:b22])
     set_param!(m, :co2cycle, :b32, p[:b32])
     set_param!(m, :co2cycle, :b33, p[:b33])
-    connect_param!(m, :co2cycle, :E, :emissions, :E, offset=0)
+    connect_param!(m, :co2cycle, :E, :emissions, :E)
 
     # RADIATIVE FORCING COMPONENT
     set_param!(m, :radiativeforcing, :forcoth, p[:forcoth])
     set_param!(m, :radiativeforcing, :fco22x, p[:fco22x])
     set_param!(m, :radiativeforcing, :eqmat, p[:eqmat])
-    connect_param!(m, :radiativeforcing, :MAT, :co2cycle, :MAT, offset=0)
+    connect_param!(m, :radiativeforcing, :MAT, :co2cycle, :MAT)
 
     # CLIMATE DYNAMICS COMPONENT
     set_param!(m, :climatedynamics, :fco22x, p[:fco22x])
@@ -75,7 +75,7 @@ function constructdice(p)
     set_param!(m, :climatedynamics, :c1, p[:c1])
     set_param!(m, :climatedynamics, :c3, p[:c3])
     set_param!(m, :climatedynamics, :c4, p[:c4])
-    connect_param!(m, :climatedynamics, :FORC, :radiativeforcing, :FORC, offset=0)
+    connect_param!(m, :climatedynamics, :FORC, :radiativeforcing, :FORC)
 
     # DAMAGES COMPONENT
     set_param!(m, :damages, :a1, p[:a1])
@@ -83,8 +83,8 @@ function constructdice(p)
     set_param!(m, :damages, :a3, p[:a3])
     set_param!(m, :damages, :damadj, p[:damadj])
     set_param!(m, :damages, :usedamadj, p[:usedamadj])
-    connect_param!(m, :damages, :TATM, :climatedynamics, :TATM, offset=0)
-    connect_param!(m, :damages, :YGROSS, :grosseconomy, :YGROSS, offset=0)
+    connect_param!(m, :damages, :TATM, :climatedynamics, :TATM)
+    connect_param!(m, :damages, :YGROSS, :grosseconomy, :YGROSS)
 
     # NET ECONOMY COMPONENT
     set_param!(m, :neteconomy, :cost1, p[:cost1])
@@ -94,8 +94,8 @@ function constructdice(p)
     set_param!(m, :neteconomy, :pbacktime, p[:pbacktime])
     set_param!(m, :neteconomy, :S, p[:S])
     set_param!(m, :neteconomy, :l, p[:l])
-    connect_param!(m, :neteconomy, :YGROSS, :grosseconomy, :YGROSS, offset=0)
-    connect_param!(m, :neteconomy, :DAMAGES, :damages, :DAMAGES, offset=0)
+    connect_param!(m, :neteconomy, :YGROSS, :grosseconomy, :YGROSS)
+    connect_param!(m, :neteconomy, :DAMAGES, :damages, :DAMAGES)
 
     # WELFARE COMPONENT
     set_param!(m, :welfare, :l, p[:l])
@@ -103,7 +103,7 @@ function constructdice(p)
     set_param!(m, :welfare, :rr, p[:rr])
     set_param!(m, :welfare, :scale1, p[:scale1])
     set_param!(m, :welfare, :scale2, p[:scale2])
-    connect_param!(m, :welfare, :CPC, :neteconomy, :CPC, offset=0)
+    connect_param!(m, :welfare, :CPC, :neteconomy, :CPC)
 
     return m
 
