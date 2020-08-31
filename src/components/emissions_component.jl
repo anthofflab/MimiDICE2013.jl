6ad99adecbaa832025918183b69aa6af9de66eee
gianeconomics <70498347+gianeconomics@users.noreply.github.com>
2020-08-30 23:18:57 -0400

    Update emissions_component.jl
    The emissions component is expanded, three new functions are included to compute etree, sigma, and gsigma, therefore sigma0, gsigma0, dersig, eland0, deland, expcost2, partfract, pbacktime0 are included as input variables. Also, a connection between the neteconomy component and this one, is created through the parameters CPRICE and PBACKTIME.

--- src/components/emissions_component.jl
+++ src/components/emissions_component.jl
@@ -2,20 +2,54 @@
     CCA     = Variable(index=[time])    #Cumulative indiustrial emissions
     E       = Variable(index=[time])    #Total CO2 emissions (GtCO2 per year)
     EIND    = Variable(index=[time])    #Industrial emissions (GtCO2 per year)
+    SIGMA   = Variable(index=[time])    #CO2-equivalent-emissions output ratio
+    GSIGMA  = Variable(index=[time])    #growth of sigma per period
+    ETREE   = Variable(index=[time])    #Emissions from deforestation
 
-    etree   = Parameter(index=[time])   #Emissions from deforestation
-    MIU     = Parameter(index=[time])   #Emission control rate GHGs
-    sigma   = Parameter(index=[time])   #CO2-equivalent-emissions output ratio
-    YGROSS  = Parameter(index=[time])   #Gross world product GROSS of abatement and damages (trillions 2005 USD per year)
-    cca0    = Parameter()               #Initial cumulative industrial emissions
+    sigma0     = Parameter()                #Initial sigma (tC per $1000 GDP US $, 2005 prices) 2005
+    gsigma0    = Parameter()                #Initial growth of sigma per decade
+    dersig     = Parameter()                #Decline rate of sigma (per year)
+    eland0     = Parameter()                #Carbon emissions from land 2005(GtC per decade)
+    deland     = Parameter()                #Decline rate of land emissions per decade
+    expcost2   = Parameter()                #Exponent of control cost function
+    cca0       = Parameter()                #Initial cumulative industrial emissions
+    partfract  = Parameter(index=[time])    #Fraction of emissions in control regime
+    pbacktime0 = Parameter()                #initial Backstop price
+    MIU        = Parameter(index=[time])    #Emission control rate GHGs
+    CPRICE     = Parameter(index=[time])    #Carbon price (2005$ per ton of CO2)
+    PBACKTIME  = Parameter(index=[time])    #Backstop price
+    YGROSS     = Parameter(index=[time])    #Gross world product GROSS of abatement and damages (trillions 2005 USD per year)
+
 
     function run_timestep(p, v, d, t)
+
+        #define function for GSIGMA
+        if is_first(t)
+            v.GSIGMA[t] = p.gsigma0
+        else
+            v.GSIGMA[t] = v.GSIGMA[t - 1] * (1 + p.dersig)^5
+        end
+
+        #define function for SIGMA
+        if is_first(t)
+            v.SIGMA[t] = p.sigma0
+        else
+            v.SIGMA[t] = v.SIGMA[t - 1] * exp(v.GSIGMA[t] * (gettime(t) - gettime(t-1)))
+        end
+
+        #define function for ETREE
+        if is_first(t)
+            v.ETREE[t] = p.eland0
+        else
+            v.ETREE[t] = v.ETREE[t - 1] * (1 - p.deland)
+        end
+
         #Define function for EIND
-        v.EIND[t] = p.sigma[t] * p.YGROSS[t] * (1-p.MIU[t])
-    
+        v.EIND[t] = v.SIGMA[t] * p.YGROSS[t] * (1-p.MIU[t])
+
         #Define function for E
-        v.E[t] = v.EIND[t] + p.etree[t]
-    
+        v.E[t] = v.EIND[t] + v.ETREE[t]
+
         #Define function for CCA
         if is_first(t)
             v.CCA[t] = p.cca0
