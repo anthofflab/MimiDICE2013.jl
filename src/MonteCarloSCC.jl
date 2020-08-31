41135ab698d41a8d66d610c0aa8b996534a7f824
gianeconomics <70498347+gianeconomics@users.noreply.github.com>
2020-08-31 00:52:16 -0400

    Create MonteCarloSCC.jl
    This function allows to run a Monte Carlo Simulation which calculates the SCC of a given year on every experiment and creates an excel file for the SCC and can create an excel file for the inputs.

---
+++ src/MonteCarloSCC.jl
@@ -0,0 +1,59 @@
+function monte_carlo_compute_scc(m::Model = MimiDICE2013.get_model(); year::Union{Int, Nothing} = nothing, trials = 100, output_dir = nothing, save_trials = false) 
+    dice_years = zeros(60)
+      for i in 1:60
+    if i == 1
+      dice_years[i] = 2010
+    else
+      dice_years[i] = dice_years[i - 1] +5
+      end
+    end
+    
+    year === nothing ? error("Must specify an emission year. Try `monte_carlo_compute_scc(m, year=2020)`.") : nothing
+    !(year in dice_years) ? error("Cannot compute the scc for year $year, year must be within the model's time index $dice_years.") : nothing    
+
+    output_dir = output_dir === nothing ? joinpath(@__DIR__, "../output/", "SCC $(Dates.format(now(), "yyyy-mm-dd HH-MM-SS")) MC$trials") : output_dir
+    mkpath("$output_dir/results")
+    save_trials ? trials_output_filename = joinpath(@__DIR__, "$output_dir/trials.csv") : trials_output_filename = nothing 
+
+    scc_file = joinpath(output_dir, "scc.csv")
+    open(scc_file, "w") do f 
+        write(f, "trial, SCC: $year\n")
+    end
+    
+    mcs = MimiDICE2013.getsimv2()
+    last_year = dice_years[end]
+    mm = MimiDICE2013.get_marginal_model(year=2030)
+    
+    function scc_calculation(mcs::SimulationInstance, trialnum::Int, ntimesteps::Int, ::Nothing)
+
+
+        mm = mcs.models[1]
+
+        ntimesteps = findfirst(isequal(last_year), dice_years)     # Will run through the timestep of the specified last_year
+        run(mm, ntimesteps=ntimesteps)
+
+        marginal_damages = -1 * mm[:neteconomy, :C][1:ntimesteps] * 10^12     # Go from trillion$ to $; multiply by -1 so that damages are positive; pulse was in CO2 so we don't need to multiply by 12/44
+
+        cpc = mm.base[:neteconomy, :CPC]
+        eta = mm.base[:welfare, :elasmu]
+        prtp = mm.base[:welfare, :prtp]
+
+        year_index = findfirst(isequal(2030), dice_years)
+
+        df = [zeros(year_index-1)..., ((cpc[year_index]/cpc[i])^eta * 1/(1+prtp)^(t-2030) for (i,t) in enumerate(dice_years) if year<=t<=last_year)...]
+        scc = sum(df .* marginal_damages * 5)
+
+        open(scc_file, "a") do f
+            write(f, "$trialnum, $scc\n")
+        end
+
+        return nothing
+    end
+    
+    scc_data = run(mcs, mm, trials;
+             trials_output_filename = trials_output_filename,             
+             results_output_dir = "$output_dir/results",
+             post_trial_func = scc_calculation)
+    
+    return scc_data
+end
